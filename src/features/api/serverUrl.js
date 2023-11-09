0d82eeae7bc1b5d77177237aa8b2da74ed606bf1
BrianLupu93 <brianlupu@gmail.com>
2023-11-09 21:18:11 +0100

    url

--- src/features/api/serverUrl.js
+++ src/features/api/serverUrl.js
@@ -1,9 +1,13 @@
-export const authUrl = 'https://smart-api-afc6.onrender.com/api/v1/users';
-export const clientsUrl = 'https://smart-api-afc6.onrender.com/api/v1/clients';
-export const bookingUrl = 'https://smart-api-afc6.onrender.com/api/v1/bookings';
-export const incomesUrl = 'https://smart-api-afc6.onrender.com/api/v1/incomes';
+export const authUrl =
+  'https://fullstack-project-api.onrender.com/api/v1/users';
+export const clientsUrl =
+  'https://fullstack-project-api.onrender.com/api/v1/clients';
+export const bookingUrl =
+  'https://fullstack-project-api.onrender.com/api/v1/bookings';
+export const incomesUrl =
+  'https://fullstack-project-api.onrender.com/api/v1/incomes';
 export const subscriptionsUrl =
-  'https://smart-api-afc6.onrender.com/api/v1/subscriptions';
+  'https://fullstack-project-api.onrender.com/api/v1/subscriptions';
 
 // export const authUrl = 'http://localhost:3000/api/v1/users';
 // export const clientsUrl = 'http://localhost:3000/api/v1/clients';
