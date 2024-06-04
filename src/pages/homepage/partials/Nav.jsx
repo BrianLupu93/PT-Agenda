4b7803399a46a459839afe4ab12aee24146ad40d
Brian Lupu <brianlupu@gmail.com>
2024-06-04 22:50:47 +0200

    button test nav

--- src/pages/homepage/partials/Nav.jsx
+++ src/pages/homepage/partials/Nav.jsx
@@ -6,12 +6,14 @@ const Nav = () => {
   const navigate = useNavigate();
   return (
     <div className='w-full h-[50px] flex items-center sticky top-0 text-blue-500 bg-zinc-950 shadow-lg shadow-rose-500/70 2xl:px-40 md:px-20 px-2'>
-      <Button
-        label='TRY NOW'
-        onClick={() => navigate("/pt-agenda")}
-        extraClass='w-[150px] absolute 2xl:right-40 md:right-20 right-2 animate-pulse'
-        small
-      />
+      <div className='w-[150px] absolute 2xl:right-40 md:right-20 right-2'>
+        <Button
+          label='TRY NOW'
+          onClick={() => navigate("/pt-agenda")}
+          extraClass='animate-pulse'
+          small
+        />
+      </div>
     </div>
   );
 };
